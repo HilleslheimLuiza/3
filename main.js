5d28b9b9ee0f6496bbe3688aa3c7890c85c7c4a2
Hilleslheim <108075498+HilleslheimLuiza@users.noreply.github.com>
2024-04-16 14:38:50 -0300

    16-04

--- main.js
+++ main.js
@@ -37,14 +37,18 @@ function calculaTempo(tempoObjetivo){
     horas %= 24;
 
     if(tempoFinal > 0){
-    return dias + " dias "+ horas + " horas "+ minutos + " minutos "+ segundos + " segundos ";
+    return [dias,horas,minutos,segundos];
 } else{
-  return "TEMPO ESGOTADO";
+  return [0,0,0,0];
 }
 }
 function atualizaCronometro(){
+    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
 for (let i = 0; i<contadores.length; i++){
-    contadores[i].textContent = calculaTempo(tempos[i]);
+    //contadores[i].textContent = calculaTempo(tempos[i]);
     }
 }
  
@@ -52,4 +56,4 @@ function comecaCronometro(){
 atualizaCronometro();
 setInterval(atualizaCronometro, 1000);
 }
-//comecaCronometro();
+comecaCronometro();
